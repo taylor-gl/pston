0dcf4d58716585ca421c858679e8364644f5a21a
Taylor G. Lunt <taylor@taylor.gl>
2025-06-24 20:14:53 -0400

    Switch local dev URL to 127.0.0.1
    Stops me from clobbering other localhost project cookies.

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit(), devtoolsJson()]
+	plugins: [sveltekit(), devtoolsJson()],
+	server: {
+		host: '127.0.0.1',
+		port: 5173
+	}
 });
